edec912e04863d73103763e43c559338660a6316
Andrew Mather <Andy-mather@live.co.uk>
2018-10-11 10:07:46 +0100

    listen added

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const router = require('./router/index.js');
 
 app.use('/api', router);
 
+// none existent routes
 app.use('/*', (req, res, next) => next({ status: 404, msg: 'Page Not Found'});
+
+
 
 module.exports = { app };
