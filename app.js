ca7a092b2adba490ef19de6f9ec2014939781881
Andrew Mather <Andy-mather@live.co.uk>
2018-10-30 11:22:18 +0000

    added cors middleware

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const DB_URL = process.env.DB_URL || require('./config').DB_URL;
 const apiRouter = require('./router/index');
 const bodyParser = require('body-parser');
 const { handle400, handle404, handle500 } = require('./error-handles');
 
 mongoose.connect(DB_URL, { useNewUrlParser: true });
+app.use(cors());
 app.use(bodyParser.json());
 app.get('/', apiRouter);
 app.use('/api', apiRouter);
