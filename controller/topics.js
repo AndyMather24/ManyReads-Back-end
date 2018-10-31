48e3106e42d1dcb47dfc7f0715045a9c0e306780
Andrew Mather <Andy-mather@live.co.uk>
2018-10-31 20:42:59 +0000

    added populate

--- controller/topics.js
+++ controller/topics.js
@@ -1,33 +1,34 @@
-const { Topic, Article } = require('../models')
+const { Topic, Article } = require('../models');
 
 exports.getTopics = (req, res, next) => {
-  Topic.find()
-    .then(topics => {
-      res.send({ topics })
-    })
-    .catch(next)
-}
+	Topic.find()
+		.then((topics) => {
+			res.send({ topics });
+		})
+		.catch(next);
+};
 
 exports.getArtsForTopic = (req, res, next) => {
-  const { topic_slug } = req.params
-  Article.find({ belongs_to: topic_slug })
-    .then(articles => {
-      if (articles.length === 0) return Promise.reject({ status: 404, msg: 'Invalid Param' })
-      res.send({ articles })
-    })
-    .catch(next)
-}
+	const { topic_slug } = req.params;
+	Article.find({ belongs_to: topic_slug })
+		.populate('created_by')
+		.then((articles) => {
+			if (articles.length === 0) return Promise.reject({ status: 404, msg: 'Invalid Param' });
+			res.send({ articles });
+		})
+		.catch(next);
+};
 
 exports.addArticleToTopic = (req, res, next) => {
-  const { topic_slug } = req.params
-  const article = new Article({ ...req.body, belongs_to: topic_slug })
-  article
-    .save()
-    .then(art => {
-      res.status(201)
-      res.send(`${art.title} added successfully`)
-    })
-    .catch(err => {
-      next({ status: 400, msg: 'invalid post request' })
-    })
-}
+	const { topic_slug } = req.params;
+	const article = new Article({ ...req.body, belongs_to: topic_slug });
+	article
+		.save()
+		.then((art) => {
+			res.status(201);
+			res.send(`${art.title} added successfully`);
+		})
+		.catch((err) => {
+			next({ status: 400, msg: 'invalid post request' });
+		});
+};
